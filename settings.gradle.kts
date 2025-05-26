c87574dd9bab32aec8db2a0dce38b88eb6165d70
Jonathan Alfonso <alfonso.jonathan@protonmail.com>
2025-05-26 23:39:50 +0200

    fix dependances

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -17,12 +17,6 @@ dependencyResolutionManagement {
         google()
         mavenCentral()
     }
-
-    versionCatalogs {
-        create("libs") {
-            from(files("gradle/libs.versions.toml"))
-        }
-    }
 }
 
 rootProject.name = "HelpAbroad"
