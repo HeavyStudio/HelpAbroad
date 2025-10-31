5ee51be17bbc24743e7b6d39987b5d774d6d9180
Jonathan Alfonso <jonathan.alfonso@lundimatin.fr>
2025-10-31 14:14:11 +0100

    fix(database): Fixed db_seeds and bump to versionCode 6 for Play Store

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,7 +18,7 @@ android {
         applicationId = "com.heavystudio.helpabroad"
         minSdk = 29
         targetSdk = 36
-        versionCode = 5
+        versionCode = 6
         versionName = "0.2.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
